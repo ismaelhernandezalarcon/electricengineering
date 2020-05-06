520cb7493fe385bc1ad89f9de3248a0ce5610376
ismaelhernandezalarcon <64413593+ismaelhernandezalarcon@users.noreply.github.com>
2020-05-06 13:43:05 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,5 +3,5 @@ var Menu = document.getElementById("menu");
 var nav = document.getElementById("nav");
 
 Menu.addEventListener("click",function(){
-  nav.classList.toggle("aparecer");
+  nav.classList.toggle("mostrar");
 });
